6e98e7c80428e7f61b51c26fae9a33086bdf3e02
Courtney Waller <wllr.crtny.jyc@gmail.com>
2016-05-02 22:53:24 +0000

    functionality update - disconnect, words, hidden mode, points

--- public/main.js
+++ public/main.js
@@ -4,51 +4,71 @@ var socket = io();
 
 var pictionary = function() {
     var canvas, context, drawing, drawer, guessBox, word, me;
+    var incognitoMode = false;
     var username = prompt('What\'s your name?') || "Guest";
 
     //Update userlist 
-   var updateUsers = function(users) {
+    var updateUsers = function(users) {
         $('#users').empty();
-        users.forEach(function(user){
-            if(user.name != username){
-                 addUser(user);
+        users.forEach(function(user) {
+            if (user.name != username) {
+                addUser(user);
             } else {
-                me = user
+                me = user;
+                updatePoints(me.points);
+                if(me.drawer){
+                    setWord(me.word); 
+                    if(me.points > 20 && !incognitoMode){
+                        incognitoMode = true;
+                        $('#incognito').show();
+                    }
+                } else {
+                    $('#word').hide();
+                    $('#guess').show();
+                }
             }
+            return me;
         });
-   };
+    };
+    // update points
+    var updatePoints = function(points){
+        $('#points').text(points);
+    }
     //Add users
     var addUser = function(user) {
         var isDrawer = "notDrawing";
-        if(user.drawer){
+        if (user.drawer) {
             isDrawer = "isDrawing";
         }
-        $('#users').append('<div><button id="'+ user.id +'" class="'+isDrawer+'">' + user.name + '</button></div>');
+        $('#users').append('<div><button id="' + user.id + '" class="' + isDrawer + '">' + user.name + '</button></div>');
     };
-    $('#users').on('click','button', function() {
-        console.log("Clicked");
-        if (me.drawer){
+    // Selecting the winner
+    $('#users').on('click', 'button', function() {
+        if (me.drawer) {
             var uName = $(this).text();
             var uId = $(this).attr('id');
-            
-            // $('#guesses').append(uName + " is the winner! The word was " + word);
-            // setDrawer(uName);
-            clear()
+            clear();
             socket.emit('pickWinner', uId);
-            console.log('pickWinner', uId);
+            socket.emit('clearCanvas')
         }
     });
-    
-    var showWinner = function(user){
-        $('#guesses').append(user.name + " is the winner! The word was " + word);
-    }
-//Drawing 
+//Displaying winner message
+    var showWinner = function(obj) {
+            $('#guesses').append(obj.newDrawer.name + " is the winner! The word was " + obj.word +'<br/>').show();
+        }
+        
+        //Displaying word to draw
+        var setWord = function(word) {
+            $('#word').text("You are the drawer! Your word is: " + word).show();
+            $('#guess').hide();
+        }
+        //Drawing 
     var draw = function(position) {
         context.beginPath();
         context.arc(position.x, position.y, 6, 0, 2 * Math.PI);
         context.fill();
     };
-    var clear = function(){
+    var clear = function() {
         context.clearRect(0, 0, canvas[0].width, canvas[0].height);
     }
 
@@ -57,7 +77,7 @@ var pictionary = function() {
     canvas[0].width = canvas[0].offsetWidth;
     canvas[0].height = canvas[0].offsetHeight;
     canvas.on('mousedown', function() {
-        if(me.drawer){
+        if (me.drawer) {
             drawing = true;
         }
     });
@@ -71,19 +91,26 @@ var pictionary = function() {
             y: event.pageY - offset.top
         };
         if (drawing) {
-            draw(position);
+            if (!incognitoMode){
+                draw(position); 
+            }
             socket.emit('draw', position);
             // this is the event that listens for other drawers
         }
     });
-        
-//Clearing the canvas    
-   $('#clear').click(clear);
+    $('#clear').on('click', function() {
+        if(me.drawer){
+            clear();
+            socket.emit('clearCanvas')            
+        }
 
-//Guesses
+    });
+
+
+    //Guesses
     // Show guess history
-    var addGuess = function(guess) {
-        $('#guesses').append('<div>' + guess + '</div>');
+    var addGuess = function(obj) {
+        $('#guesses').append('<div>' + obj.user + ": " + obj.guess + '</div>');
         console.log(guess);
     };
     //Capture guess input, return input
@@ -92,47 +119,20 @@ var pictionary = function() {
         if (event.keyCode != 13) {
             return;
         }
-        
+
         var guess = guessBox.val();
-        addGuess(guess);
+        addGuess({user:'Me',guess:guess});
         socket.emit('guess', guess);
         guessBox.val('');
     });
-    
+
     socket.emit('addUser', username);
-    socket.on('draw', draw);    
+    socket.on('draw', draw);
     socket.on('updateUsers', updateUsers);
     socket.on('guess', addGuess);
-    socket.on('showWinner',showWinner);
+    socket.on('showWinner', showWinner);
+    socket.on('clearCanvas', clear);
 };
-       
+
 
 $(document).ready(pictionary);
-
-//Set Drawer    
-/*var setDrawer = function(user) {
-    if (username == user) {
-    drawer = true;
-    //console.log(username);
-} 
-};
-
-socket.on('setDrawer', setDrawer);
-
-//Pick winner
-$('.nameButton').on('click', function() {
-    console.log("Clicked");
-    var uName = $(this).text();
-    
-    setDrawer(uName);
-    context.clearRect(0, 0, canvas[0].width, canvas[0].height);
-    socket.emit('pickWinner', uName);
-    console.log('pickWinner', uName);
-});
-
-socket.on('pickWinner', setDrawer ); 
-
-//Update drawer screen
-if (drawer) {
-    $('#clear').show();
-}*/
--- server.js
+++ server.js
@@ -7,15 +7,36 @@ app.use(express.static('public'));
 var server = http.Server(app);
 var io = socket_io(server);
 var usersArray = [];
-var userIndex = {};
+// var usersIndex = {};
 var lastId = 0
 var drawer = null;
+var WORDS = [
+    "lion", "sock", "number", "person", "pen", "banana", "people",
+    "song", "water", "side", "map", "man", "pool table", "woman", "bathroom", "boy",
+    "girl", "circus", "cowboy", "roller skates", "laptop", "name", "sentence", "line", "air",
+    "toast", "home", "hand", "house", "picture", "animal", "mother", "father",
+    "bicycle", "lightsaber", "world", "head", "page", "airplane", "fishing",
+    "exam", "school", "plant", "food", "sun", "dinosaur", "eye", "city", "tree",
+    "farm", "book", "sea", "key", "salt and pepper", "bacon", "movie", "south", "east",
+    "couch", "child", "children", "rocket", "paper", "music", "river", "car",
+    "foot", "round", "book", "baseball", "bear", "king", "queen", "curtains",
+    "mountain", "horse", "watch", "color", "face", "wood", "list", "bird",
+    "body", "dog", "family", "worm", "door", "mountain", "wind", "ship", "dart",
+    "rock", "fast food", "fire", "bookshelf", "piece", "pirate", "castle", "teapot",
+    "space"
+];
+
+function wordPicker(wordList) {
+    return wordList[Math.floor(Math.random()*(wordList.length-1))];
+};
 
   function addUser(username) {
       var user = {
           name: username,
           id: lastId++,
-          drawer:false
+          drawer:false,
+          word:null,
+          points: 0
       }
   usersArray.push(user);
 //   usersIndex[user.id] = user;
@@ -26,37 +47,46 @@ var drawer = null;
   
   return user.id;
 }  
-function removeUser(user) {
-//   user = usersIndex[user.id];
-  
-//   if (user) {
-//     var userIndex = usersArray.indexOf(user);
-//     usersArray.splice(userIndex, 1);
-//     if (drawer === user) {
-//       var nextUser = usersArray[userIndex];
+function removeUser(userID) {
+    //console.log("user" + userID);
+   //user = usersArray[user.id - 1];
+     if (userID) {
+        var userIndex;
+        usersArray.forEach(function(user,index){
+            if(user.id == userID){
+                userIndex = index
+            }
+        });
+    // var userIndex = usersArray.indexOf(username);
+    usersArray.splice(userIndex, 1);
+    if (drawer.id === userID) {
+       var nextUser = usersArray[userIndex];
     
-//       if (nextUser) {
-//         setDrawer(nextUser);
-//       }
-//     }
-//   }
+       if (nextUser) {
+          setDrawer(nextUser);
+       } else {
+          setDrawer(usersArray[0])
+       }
+     }
+   }
 }
 function setDrawer(user) {
-    console.log(user);
     usersArray = usersArray.map(function(userObj){
-        console.log(user,userObj);
         if(userObj.id == user.id){
-           userObj.drawer = true;   
+           userObj.drawer = true;
+           userObj.word = wordPicker(WORDS);
         } else{
             userObj.drawer = false;
+            userObj.word = null;
         }
         return userObj;
     });
   
   drawer = user;
+//  var word = 
 }
 function getDrawerIndex(id){
-    console.log('id',id)
+    //console.log('id',id)
     var currentDrawerIndex;
     usersArray.forEach(function(user,index){
             if(user.id == id){
@@ -69,7 +99,7 @@ function getDrawerIndex(id){
 io.on('connect', function(socket) {
    // console.log('Client connected');
    socket.on('addUser', function(username) {
-        addUser(username);
+        socket.userID = addUser(username); 
         socket.emit('updateUsers',usersArray)
         socket.broadcast.emit('updateUsers', usersArray)
    })
@@ -79,18 +109,31 @@ io.on('connect', function(socket) {
     });
     
     socket.on('guess', function(guessBox){
-        socket.broadcast.emit('guess', guessBox);
+        var guesserindex = getDrawerIndex(socket.userID);
+        socket.broadcast.emit('guess', {user: usersArray[guesserindex].name, guess:guessBox});
     })
   
    socket.on('pickWinner', function(uId) {
        //change the current drawer
+       var word = drawer.word;
        var newDrawer = usersArray[getDrawerIndex(uId)]
+       newDrawer.points += 10;
        setDrawer(newDrawer);
        socket.emit('updateUsers',usersArray)
        socket.broadcast.emit('updateUsers',usersArray)
        //broadcast the list changed && broadcast the winner
-      socket.emit('showWinner', newDrawer);
-      socket.broadcast.emit('showWinner', newDrawer);
+      socket.emit('showWinner', {newDrawer:newDrawer, word:word});
+      socket.broadcast.emit('showWinner', {newDrawer:newDrawer, word:word});
+   }) 
+   socket.on('clearCanvas', function () {
+             socket.emit('clearCanvas')
+       socket.broadcast.emit('clearCanvas')
+   })
+   socket.on('disconnect', function(event){
+       // get the info about the socket
+        removeUser(socket.userID);
+        socket.emit('updateUsers',usersArray)
+        socket.broadcast.emit('updateUsers', usersArray)
    })
       
 });
